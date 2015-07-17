b1a5cdfc2de6b4d2ef8e669871506c1a4976b547
andreipapuc <papuc.apc@gmail.com>
2015-07-17 10:10:43 +0300

    Update AdminMainActivity.java

--- app/src/main/java/com/andreicarlopapuc/huntinglupus/AdminMainActivity.java
+++ app/src/main/java/com/andreicarlopapuc/huntinglupus/AdminMainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class AdminMainActivity extends ActionBarActivity {
 
