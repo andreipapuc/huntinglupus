1654da39546fdb98a72813b49e6ff384953ffd34
Alex Manolache <alexmanolachedragos@yahoo.com>
2015-07-25 01:43:57 +0300

    fuck this renaming shit... - alex

--- app/src/main/java/com/andreicarlopapuc/huntinglupus/UserAllProductsEXPANDABLElist.java
+++ app/src/main/java/com/andreicarlopapuc/huntinglupus/UserAllProductsEXPANDABLElist.java
@@ -5,32 +5,32 @@ package com.andreicarlopapuc.huntinglupus;
  */
 
 import java.util.ArrayList;
-        import java.util.HashMap;
-        import java.util.LinkedHashMap;
-        import java.util.List;
-        import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
-        import com.andreicarlopapuc.huntinglupus.adapters.ExpandableListAdapter;
+import com.andreicarlopapuc.huntinglupus.adapters.ExpandableListAdapter;
 
-        import android.app.Activity;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.os.StrictMode;
-        import android.util.DisplayMetrics;
-        import android.util.Log;
-        import android.view.Menu;
-        import android.view.View;
-        import android.widget.ExpandableListView;
-        import android.widget.ExpandableListView.OnChildClickListener;
-        import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.Toast;
 
-        import org.apache.http.NameValuePair;
-        import org.json.JSONArray;
-        import org.json.JSONException;
-        import org.json.JSONObject;
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
-public class UserAllProductsEXPANDABLElist extends Activity {
+public class UserAllProductsExpandableList extends Activity {
 
     List<String> groupList;
     List<String> childList;
